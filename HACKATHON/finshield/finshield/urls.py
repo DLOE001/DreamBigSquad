70e4c310296165ae189d557346740e581c253024
viceroi <107919050+viceroi@users.noreply.github.com>
2025-02-28 12:12:51 +0800

    help me jesus my lord

--- HACKATHON/finshield/finshield/urls.py
+++ HACKATHON/finshield/finshield/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for sms_scam_checker project.
+URL configuration for finshield project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
